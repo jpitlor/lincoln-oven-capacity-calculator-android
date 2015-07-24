dfb626298a5ed596f86c2a5ce34f16fb932df624
Jordan <jpitlor@gmail.com>
2015-07-24 10:56:52 -0400

    improved main menu - extracted styles into a theme and ensured a good luck of the manitowoc logo on all screen sizes
    also added spinning progress bar to embedded pdfs

--- app/src/main/java/com/manitowocfoodservice/lincolnovencapacitycalculator/SpecSheet.java
+++ app/src/main/java/com/manitowocfoodservice/lincolnovencapacitycalculator/SpecSheet.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class SpecSheet extends Activity {
 	@Override
@@ -16,28 +18,23 @@ public class SpecSheet extends Activity {
 		webView.getSettings().setJavaScriptEnabled(true);
 		String id = getIntent().getStringExtra("id");
 		String pdf = "http://www.lincolnfp.com/asset/?id=" + id;
+
+		webView.setWebViewClient(new WebViewClient() {
+			public void onPageFinished(WebView view, String url) {
+				findViewById(R.id.progressBar).setVisibility(View.GONE);
+			}
+		});
+
 		webView.loadUrl("http://docs.google.com/gview?embedded=true&url=" + pdf);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_spec_sheet, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
 		return super.onOptionsItemSelected(item);
 	}
 }
